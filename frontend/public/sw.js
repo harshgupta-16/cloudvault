27628215c9cc0a23ea1f5e8ee23ecdb2db378c76
hasrhgupta-16 <electromen2005@gmail.com>
2026-01-07 21:41:09 +0530

    bug fixing

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cloudvault-v1";
+const CACHE_NAME = "cloudvault-v2";
 
 const STATIC_ASSETS = [
     "/",
@@ -21,9 +21,26 @@ self.addEventListener("activate", () => {
 
 // Fetch
 self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") return;
+
     event.respondWith(
-        caches.match(event.request).then(cached => {
-            return cached || fetch(event.request);
+        caches.match(event.request).then((cached) => {
+            return (
+                cached ||
+                fetch(event.request).catch(() => {
+                    // fallback for navigation
+                    if (event.request.mode === "navigate") {
+                        return caches.match("/");
+                    }
+                })
+            );
         })
     );
+});
+
+
+self.addEventListener("message", (event) => {
+    if (event.data === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
